2644db99badf3d58be268add7ec02359395de365
Gaurav799 <gauravsinghkholiya123@gmail.com>
2022-06-15 08:41:39 +0530

    Update example5.py

--- example5.py
+++ example5.py
@@ -2,12 +2,12 @@ from turtle import *
 
 fillcolor('yellow')
 begin_fill()
-for i in range(5):
+for i in range(6):
     fd(100)
-    lt(72)
+    lt(60)
 end_fill()
 fillcolor('red')
 begin_fill()
-circle(50)
+circle(40)
 end_fill()
 mainloop()
